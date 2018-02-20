e8281ff15dd8941866cf4a7a544eb0bb0c4256a0
jcb1317 <35781997+jcb1317@users.noreply.github.com>
2018-02-20 20:02:59 +0100

    Added `basic_cmd` parameter
    so command names can be changed more easily

--- commands.py
+++ commands.py
@@ -22,7 +22,7 @@ import async_timeout
 import wikipedia
 from random import choice
 
-async def eightball(client, message):
+async def eightball(client, message, basic_cmd):
     ans = choice(resources["messages"]["8ball_answers"])
     em = discord.Embed(title=ans, color=colors[0])
     await send(client, message.channel, em)
@@ -42,12 +42,12 @@ async def send(client, channel, embed):
     except discord.errors.Forbidden:
         pass
 
-async def bot_info(client, message):
+async def bot_info(client, message, basic_cmd):
     em = discord.Embed(title=resources["messages"]["info"], color=colors[0])
     em.set_author(name=client.user.name, icon_url=client.user.avatar_url)
     await send(client, message.channel, em)
 
-async def help_command(client, message):
+async def help_command(client, message, basic_cmd):
     split = message.content.split(" ")
     if len(split) == 1:
         helplist = ", ".join(list(resources["commands"].keys()))
@@ -66,15 +66,15 @@ async def help_command(client, message):
                                description=resources["messages"]["help_error"].format(prefix + split[1]),
                                color=colors[1])
     else:
-        desc = resources["messages"]["invalid_arguments"].format("'" + prefix + "help' or '" + prefix + "help wiki'")
+        desc = resources["messages"]["invalid_arguments"].format("'" + prefix + basic_cmd + "' or '" + prefix + basic_cmd + " wiki'")
         em = discord.Embed(title="Error", description=desc, color=colors[1])
     await send(client, message.channel, em)
 
 
-async def wiki(client, message):
-    if len(message.content) > len(prefix) + len("wiki") + 1:
+async def wiki(client, message, basic_cmd):
+    if len(message.content) > len(prefix) + len(basic_cmd) + 1:
         try:
-            cur_req = wikipedia.page(title=message.content[len(prefix) + len("wiki") + 1:])
+            cur_req = wikipedia.page(title=message.content[len(prefix) + len(basic_cmd) + 1:])
             content = ""
             if len(cur_req.summary) >= 400:
                 content = cur_req.summary[:400] + "..."
@@ -85,25 +85,25 @@ async def wiki(client, message):
             em = discord.Embed(description=str(e))
         except:
             em = discord.Embed(description=resources["messages"]["wiki_not_found"].format(
-                message.content[len(prefix) + len("wiki") + 1:]), color=colors[1])
+                message.content[len(prefix) + len(basic_cmds) + 1:]), color=colors[1])
         await send(client, message.channel, em)
 
 
-async def setstatus(client, message):
-    if message.author.id == resources["core"]["bot_owner_id"] and len(message.content) > len(prefix) + 7:
+async def setstatus(client, message, basic_cmd):
+    if message.author.id == resources["core"]["bot_owner_id"] and len(message.content) > len(prefix) + 1 + len(basic_cmd):
         await client.change_presence(game=discord.Game(name=(message.content[9:])))
     else:
         await send(client, message.channel, discord.Embed(title=resources["messages"]["wrong_perms"], color=colors[1]))
 
 
-async def purge(client, message):
+async def purge(client, message, basic_cmd):
     mc = False
     for i in message.author.roles:
         if i.permissions.manage_channels:
             mc = True
         elif i.permissions.administrator:
             mc = True
-    if mc and len(message.content) > len(prefix) + 6:
+    if mc and len(message.content) > len(prefix) + len(basic_cmd) + 1:
         try:
             int(message.content[len(prefix) + 6:])
             await client.purge_from(message.channel, limit=int(message.content[len(prefix) + 6:]))
@@ -112,8 +112,8 @@ async def purge(client, message):
                                color=colors[1])
             await send(client, message.channel, em)
     elif mc:
-        em = discord.Embed(title=resources["messages"]["invalid_arguments"].format("'" + prefix + "clear 50'"),
+        em = discord.Embed(title=resources["messages"]["invalid_arguments"].format("'" + prefix + basic_cmd + " 50'"),
                            color=colors[1])
         await send(client, message.channel, em)
-    elif len(message.content) > len(prefix) + 6:
+    elif len(message.content) > len(prefix) + 1 + len(basic_cmd):
         await send(client, message.channel, discord.Embed(resources["messages"]["wrong_perms"]))
